cb6d033276b6a5a40d59990ab5d365559fd49e2e
Kelv1nG <kelvinmaglipon@gmail.com>
2024-08-05 06:05:43 +0800

    formatting

--- app/core/database.py
+++ app/core/database.py
@@ -2,8 +2,7 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.settings.config import settings
 
